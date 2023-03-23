0ca84aa567709df89f94d3ccf8510952ab8f2c17
dungpdbn <dungpdbn@gmail.com>
2023-03-23 13:58:57 +0700

    23/3

--- src/App.tsx
+++ src/App.tsx
@@ -14,14 +14,12 @@ function App() {
           <img src={youtubeLogo} className="max-h-[56px] m-[14px]" />
         </div>
         <div className="flex items-center">
-          
-            <form className="flex items-center h-[40px] border border-solid pl-6 border-[#ccc] border-r-0 rounded-tl-[40px] rounded-bl-[40px]">
-              <input className="focus:outline-none"/>
-            </form>
-            <button className="m-0 h-[40px] w-[64px] border border-solid border-[#d3d3d3] bg-[#f8f8f8] rounded-tr-[40px] rounded-br-[40px]">
-              <i className="fa fa-search"></i>
-            </button>
-        
+          <form className="flex items-center h-[40px] border border-solid pl-6 border-[#ccc] border-r-0 rounded-tl-[40px] rounded-bl-[40px]">
+            <input className="focus:outline-none" />
+          </form>
+          <button className="m-0 h-[40px] w-[64px] border border-solid border-[#d3d3d3] bg-[#f8f8f8] rounded-tr-[40px] rounded-br-[40px]">
+            <i className="fa fa-search"></i>
+          </button>
         </div>
       </div>
     </>
