85a82db0a188142974e4605c09559070d8b7f048
dungpdbn <dungpdbn@gmail.com>
2023-03-23 11:36:50 +0700

    23/3

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,31 @@
-import { useState } from 'react';
-import menuBar from './assets/menu-bar.png';
-import youtubeLogo from './assets/YouTube-Logo-PNG7.png';
-import './App.css';
+import { useState } from "react";
+import menuBar from "./assets/menu-bar.png";
+import youtubeLogo from "./assets/YouTube-Logo-PNG7.png";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
-      <div className="h-[56px] flex items-center p-[16px]">
-        <img src={menuBar} className="max-h-[29px] m-[8px] "/>
-        <img src={youtubeLogo} className="max-h-[56px] m-[14px]"/>
+      <div className="flex h-[56px] px-[16px]">
+        <div className="flex items-center">
+          <img src={menuBar} className="max-h-[29px] m-[8px] " />
+          <img src={youtubeLogo} className="max-h-[56px] m-[14px]" />
+        </div>
+        <div className="flex items-center">
+          
+            <form className="flex items-center h-[40px] border border-solid pl-6 border-[#ccc] border-r-0 rounded-tl-[40px] rounded-bl-[40px]">
+              <input className="focus:outline-none"/>
+            </form>
+            <button className="m-0 h-[40px] w-[64px] border border-solid border-[#d3d3d3] bg-[#f8f8f8] rounded-tr-[40px] rounded-br-[40px]">
+              <i className="fa fa-search"></i>
+            </button>
+        
+        </div>
       </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
