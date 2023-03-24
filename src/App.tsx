2cb7ad1bb1671b04630dc91a17be8cd05c820791
dungpdbn <dungpdbn@gmail.com>
2023-03-24 13:35:48 +0700

    24/3

--- src/App.tsx
+++ src/App.tsx
@@ -8,18 +8,45 @@ function App() {
 
   return (
     <>
-      <div className="flex h-[56px] px-[16px]">
+      <div className="flex h-[56px] px-[16px] justify-between">
         <div className="flex items-center">
           <img src={menuBar} className="max-h-[29px] m-[8px] " />
           <img src={youtubeLogo} className="max-h-[56px] m-[14px]" />
         </div>
-        <div className="flex items-center">
-          <form className="flex items-center h-[40px] border border-solid pl-6 border-[#ccc] border-r-0 rounded-tl-[40px] rounded-bl-[40px]">
-            <input className="focus:outline-none" />
+        <div className="flex items-center flex-[0_1_536px]">
+          <form className="flex flex-auto items-center h-[40px] border border-solid pl-6 border-[#ccc] border-r-0 rounded-tl-[40px] rounded-bl-[40px]">
+            <input className="focus:outline-none w-full" />
           </form>
           <button className="m-0 h-[40px] w-[64px] border border-solid border-[#d3d3d3] bg-[#f8f8f8] rounded-tr-[40px] rounded-br-[40px]">
             <i className="fa fa-search"></i>
           </button>
+        </div>
+        <div className="flex items-center">
+          <button className="w-[40px] h-[40px] flex items-center justify-center">
+            <span className="material-symbols-outlined">notifications</span>
+          </button>
+          <button className="w-[40px] h-[40px] flex items-center justify-center">
+            <span className="material-symbols-outlined">notifications</span>
+          </button>
+        </div>
+      </div>
+      <div className="flex">
+        <div className="border px-[12px]">
+          <div className="flex w-[204px] rounded-[10px] items-center h-[40px] bg-[#0000000d] px-[12px]">
+            <span className="material-symbols-outlined h-[24px] w-[24px] mr-[24px]">home</span>
+            <button className="flex-auto text-left">Home</button>
+          </div>
+          <div className="flex w-[204px] rounded-[10px] items-center h-[40px] bg-[#0000000d] px-[12px]">
+            <span className="material-symbols-outlined h-[24px] w-[24px] mr-[24px]">home</span>
+            <button className="flex-auto text-left">Home</button>
+          </div>
+          <div className="flex w-[204px] rounded-[10px] items-center h-[40px] bg-[#0000000d] px-[12px]">
+            <span className="material-symbols-outlined h-[24px] w-[24px] mr-[24px]">home</span>
+            <button className="flex-auto text-left">Home</button>
+          </div>
+        </div>
+        <div className="flex-auto border">
+
         </div>
       </div>
     </>
