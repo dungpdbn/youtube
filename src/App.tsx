dfc43b6bcc923a5d126bc7e38b76a95473de330e
dungpdbn <dungpdbn@gmail.com>
2023-03-27 16:55:28 +0700

    27/3

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,22 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import menuBar from "./assets/menu-bar.png";
 import youtubeLogo from "./assets/YouTube-Logo-PNG7.png";
 import "./App.css";
+import { ReactComponent as Shorts } from "./assets/shorts.svg";
+import { ReactComponent as Subscriptions } from "./assets/subscriptions.svg";
+import { ReactComponent as Library } from "./assets/library.svg";
+const API = 'AIzaSyC3eMtziQLUNNR2fvlHpyvhqG4-FjRL_0Q';
+const channelId = 'UCxr2u-kD8QYntD9WzC_7QXg';
+let fetchUrl = `https://www.googleapis.com/youtube/v3/search?key=${API}&channelId=${channelId}&part=snippet,id&order=date&maxResults=20`
 
 function App() {
   const [count, setCount] = useState(0);
-
+  useEffect(() => {
+    fetch(fetchUrl).then((response) => response.json()).then((resJson) => {
+      console.log(resJson);
+    });
+    
+  }, [])
   return (
     <>
       <div className="flex h-[56px] px-[16px] justify-between">
@@ -31,23 +42,39 @@ function App() {
         </div>
       </div>
       <div className="flex">
-        <div className="border px-[12px]">
-          <div className="flex w-[204px] rounded-[10px] items-center h-[40px] bg-[#0000000d] px-[12px]">
-            <span className="material-symbols-outlined h-[24px] w-[24px] mr-[24px]">home</span>
-            <button className="flex-auto text-left">Home</button>
+        <div className="flex flex-col">
+          <div className="px-[12px] pt-[12px]">
+            <div className="flex w-[204px] rounded-[10px] items-center h-[40px] bg-[#0000000d] px-[12px]">
+              <span className="material-symbols-outlined h-[24px] w-[24px] mr-[24px]">
+                home
+              </span>
+              <button className="flex-auto text-left">Home</button>
+            </div>
+            <div className="flex w-[204px] rounded-[10px] items-center h-[40px] px-[12px]">
+              <Shorts className="h-[24px] w-[24px] mr-[24px]" />
+              <button className="flex-auto text-left">Shorts</button>
+            </div>
+            <div className="flex w-[204px] rounded-[10px] items-center h-[40px] px-[12px]">
+              <Subscriptions className="h-[24px] w-[24px] mr-[24px]" />
+              <button className="flex-auto text-left">Subscriptions</button>
+            </div>
           </div>
-          <div className="flex w-[204px] rounded-[10px] items-center h-[40px] bg-[#0000000d] px-[12px]">
-            <span className="material-symbols-outlined h-[24px] w-[24px] mr-[24px]">home</span>
-            <button className="flex-auto text-left">Home</button>
-          </div>
-          <div className="flex w-[204px] rounded-[10px] items-center h-[40px] bg-[#0000000d] px-[12px]">
-            <span className="material-symbols-outlined h-[24px] w-[24px] mr-[24px]">home</span>
-            <button className="flex-auto text-left">Home</button>
+          <div className="px-[12px] border-t mt-[12px] pt-[12px]">
+            <div className="flex w-[204px] rounded-[10px] items-center h-[40px] bg-[#0000000d] px-[12px]">
+              <Library className="h-[24px] w-[24px] mr-[24px]" />
+              <button className="flex-auto text-left">Library</button>
+            </div>
+            <div className="flex w-[204px] rounded-[10px] items-center h-[40px] px-[12px]">
+              <Shorts className="h-[24px] w-[24px] mr-[24px]" />
+              <button className="flex-auto text-left">Shorts</button>
+            </div>
+            <div className="flex w-[204px] rounded-[10px] items-center h-[40px] px-[12px]">
+              <Subscriptions className="h-[24px] w-[24px] mr-[24px]" />
+              <button className="flex-auto text-left">Subscriptions</button>
+            </div>
           </div>
         </div>
-        <div className="flex-auto border">
-
-        </div>
+        <div className="flex-auto border"></div>
       </div>
     </>
   );
